7e13147a700550bbdb4d09c0ed90c79ea13f16a6
Ben Tristem <github@bentristem.com>
2017-11-01 15:40:37 +0000

    Boom!

--- Assets/RustyBulletHoles.cs
+++ Assets/RustyBulletHoles.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class RustyBulletHoles : MonoBehaviour {
 
+    [SerializeField] GameObject boom;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    ParticleSystem part;
+
 	// Use this for initialization
 	void Start () {
-		
+        collisionEvents = new List<ParticleCollisionEvent>();
+        part = GetComponent<ParticleSystem>();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +22,9 @@ public class RustyBulletHoles : MonoBehaviour {
 
     void OnParticleCollision(GameObject other)
     {
-        print("Hit " + other.name);
+        part.GetCollisionEvents(other, collisionEvents);
+
+        Vector3 impactPoint = collisionEvents[0].intersection;
+        Instantiate(boom, impactPoint, Quaternion.identity);
     }
 }
